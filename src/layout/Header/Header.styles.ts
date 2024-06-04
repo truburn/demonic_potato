64f6020d3c29965b9f713cb8dc91b9e4ddf24508
Jenny Seburn <jennifer.seburn@gmail.com>
2024-06-04 18:22:00 -0400

    style header

--- src/layout/Header/Header.styles.ts
+++ src/layout/Header/Header.styles.ts
@@ -1,7 +1,32 @@
 import { css } from "@emotion/css";
+import { useTheme } from "@emotion/react";
 
 export function useHeaderStyles() {
-  const root = css({});
+  const theme = useTheme();
 
-  return { root };
+  const root = css({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 60,
+    background: theme.colors.primary,
+    color: theme.colors.contrast,
+    padding: "0.5rem 1rem",
+  });
+
+  const logoLink = css({
+    height: "100%",
+    color: theme.colors.contrast,
+    "&:hover, &:focus, &:active, &:visited": {
+      color: theme.colors.contrast,
+    },
+  });
+
+  const logo = css({
+    height: "100%",
+    fill: "currentcolor",
+  });
+
+  return { root, logoLink, logo };
 }
--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,21 @@
 import { useHeaderStyles } from "@layout/Header";
+import Logo from "@assets/logo.svg?react";
+import { Navigation } from "@layout/Navigation";
+import { Link } from "react-router-dom";
 
 export function Header() {
   const classes = useHeaderStyles();
 
-  return <header className={classes.root}>Header</header>;
+  return (
+    <header className={classes.root}>
+      <Link
+        to="/library"
+        className={classes.logoLink}
+        title="Return to library"
+      >
+        <Logo className={classes.logo} />
+      </Link>
+      <Navigation />
+    </header>
+  );
 }
