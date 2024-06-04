2b4ae6a45ce81504f5b14b1daae90a5b16bc9f96
Jenny Seburn <jennifer.seburn@gmail.com>
2024-06-04 12:55:55 -0400

    additional setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [
+      {
+        find: "@assets",
+        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      },
+      {
+        find: "@components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      },
+      {
+        find: "@form",
+        replacement: fileURLToPath(new URL("./src/form", import.meta.url)),
+      },
+      {
+        find: "@layout",
+        replacement: fileURLToPath(new URL("./src/layout", import.meta.url)),
+      },
+      {
+        find: "@pages",
+        replacement: fileURLToPath(new URL("./src/pages", import.meta.url)),
+      },
+      {
+        find: "@styles",
+        replacement: fileURLToPath(new URL("./src/styles", import.meta.url)),
+      },
+      {
+        find: "@utils",
+        replacement: fileURLToPath(new URL("./src/utils", import.meta.url)),
+      },
+    ],
+  },
+});
