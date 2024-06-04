09f8ec1a6bffda21036d2053b2f5edddcd03795e
Jenny Seburn <jennifer.seburn@gmail.com>
2024-06-04 19:01:37 -0400

    style footer

--- src/layout/Footer/Footer.styles.ts
+++ src/layout/Footer/Footer.styles.ts
@@ -1,7 +1,28 @@
 import { css } from "@emotion/css";
+import { useTheme } from "@emotion/react";
 
 export function useFooterStyles() {
-  const root = css({});
+  const theme = useTheme();
 
-  return { root };
+  const root = css({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+    padding: "0.5rem 1rem",
+    fontSize: theme.font.size.small,
+    background: theme.colors.standard,
+    color: theme.colors.contrast,
+  });
+
+  const icon = css({
+    padding: "0 1ch",
+  });
+
+  const content = css({
+    margin: 0,
+    padding: 0,
+  });
+
+  return { root, icon, content };
 }
--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
+import { Icon } from "@components/Icon";
 import { useFooterStyles } from "@layout/Footer";
+import { faCopyright } from "@awesome.me/kit-3d36d6b3e3/icons/classic/light";
 
 export function Footer() {
   const classes = useFooterStyles();
 
-  return <footer className={classes.root}>Footer</footer>;
+  return (
+    <footer className={classes.root}>
+      <p className={classes.content}>
+        Copyright
+        <Icon icon={faCopyright} className={classes.icon} />
+        2024 JSburn
+      </p>
+      <p className={classes.content}>For personal use only</p>
+    </footer>
+  );
 }
