82cbe800ef22b53c172a3cea720cc704840b54e0
Jenny Seburn <jennifer.seburn@gmail.com>
2024-06-07 16:04:54 -0400

    create error page

--- src/pages/ErrorPage/ErrorPage.styles.ts
+++ src/pages/ErrorPage/ErrorPage.styles.ts
@@ -1,7 +1,28 @@
 import { css } from "@emotion/css";
+import { useTheme } from "@emotion/react";
 
 export function useErrorPageStyles() {
-  const root = css({});
+  const theme = useTheme();
 
-  return { root };
+  const root = css({
+    flex: 1,
+    maxWidth: 850,
+    margin: "0 auto",
+  });
+
+  const paragraph = css({
+    fontFamily: theme.font.serif,
+  });
+
+  const message = css({
+    fontWeight: theme.font.weight.semiBold,
+    fontSize: theme.font.size.large,
+  });
+
+  const stackLine = css({
+    marginLeft: "1.5rem",
+    fontWeight: theme.font.weight.light,
+  });
+
+  return { root, paragraph, message, stackLine };
 }
--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,29 @@
+import { PageTitle } from "@components/PageTitle";
+import { cx } from "@emotion/css";
+import { Footer } from "@layout/Footer";
+import { Header } from "@layout/Header";
 import { useErrorPageStyles } from "@pages/ErrorPage";
+import { useRouteError } from "react-router-dom";
 
 export function ErrorPage() {
   const classes = useErrorPageStyles();
+  const error = useRouteError() as Error;
 
-  return <div className={classes.root}>Error Page</div>;
+  return (
+    <>
+    <Header />
+  <main className={classes.root}>
+    <PageTitle>An Error has occurred</PageTitle>
+    {error.stack?.split("\n").map((errLine, idx) => (
+      <p key={idx} className={cx(classes.paragraph, {
+        [classes.message]: idx === 0,
+        [classes.stackLine]: idx > 0
+      })}>
+        {errLine}
+      </p>
+    ))}
+  </main>
+  <Footer />
+  </>
+  );
 }
