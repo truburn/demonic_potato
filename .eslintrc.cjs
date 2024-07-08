9fd12e4fd878bd9e062391690995105d99766d00
Jenny Seburn <jennifer.seburn@gmail.com>
2024-07-07 22:05:16 -0400

    update linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-return": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
   },
 };
